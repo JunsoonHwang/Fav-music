0a4d7d4ec14a03e76c077f0d53f6c3b73421b872
JunsoonHwang <wnstns13@gmail.com>
2021-07-02 17:40:01 +0900

    노티스 스와이퍼 추가

--- js/main.js
+++ js/main.js
@@ -57,3 +57,11 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1
   });
 })
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  // 방향 
+  autoplay: true,
+  loop: true
+})
