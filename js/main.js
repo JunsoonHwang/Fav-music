432ef9b29653c184f6c012155dd4b6167650b31b
JunsoonHwang <wnstns13@gmail.com>
2021-07-04 17:48:59 +0900

    프로모션 파트

--- js/main.js
+++ js/main.js
@@ -65,3 +65,22 @@ new Swiper('.notice-line .swiper-container', {
   autoplay: true,
   loop: true
 })
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 추가
+    clickable: true // 페이지 번호를 클릭해서 이동할 수 있도록
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
